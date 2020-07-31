834977d17b56252713c17f819833f626cddfe18d
SStefanovskyi <sstefanovskiy@gmail.com>
2020-07-31 17:27:31 +0300

    add ship

--- alien_invasion.py
+++ alien_invasion.py
@@ -1,8 +1,9 @@
 import sys
 
-from settings import Settings
-
 import pygame
+
+from settings import Settings
+from ship import Ship
 
 class AlienInvasion:
     #Клас для управления ресурсами и поведением игры
@@ -15,6 +16,8 @@ class AlienInvasion:
         self.screen = pygame.display.set_mode(
             (self.settings.screen_width, self.settings.screen_height))
         pygame.display.set_caption("Alien Invasion")
+
+        self.ship = Ship(screen)
 
         #Назначение цвета фона
         self.bg_color = (230, 230, 230)
@@ -29,6 +32,7 @@ class AlienInvasion:
 
             #При каждом проходе цикла прорисовывается экран
             self.screen.fill(self.settings.bg_color)
+            self.ship.blitme()
 
             #Отображение последнего прорисованого эерана
             pygame.display.flip()
---
+++ ship.py
@@ -0,0 +1,19 @@
+import pygame
+
+
+class Ship():
+    # Клас управления кораблем
+    def __init__(self, ai_game):
+        # Инициализирует корабыль и задает его начальную позицию
+        self.screen = ai_game.screen
+        self.screen_rect = ai_game.screen.get_rect()
+
+        # Загружает изображение корабля и получает прямоугольник
+        self.image = pygame.image.load('images/ship.bmp')
+        self.rect = self.image.get_rect()
+        # Каждый новыий корабль появляется у нижнего края экрана
+        self.rect.midbottom = self.screen_rect.midbottom
+
+    def blitme(self):
+        # Рисует корабль в текущей позиции
+        self.screen.blit(self.image, self.rect)
