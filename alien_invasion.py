fb24394ac6034b28d2a3442d8c7d50ff505477fa
SStefanovskyi <sstefanovskiy@gmail.com>
2020-08-08 14:52:27 +0300

    add grupping bullets

--- alien_invasion.py
+++ alien_invasion.py
@@ -12,11 +12,15 @@ class AlienInvasion:
         pygame.init()
         self.settings = Settings()
 
+        '''self.screen = pygame.display.set_mode((0, 0), pygame.FULLSCREEN)
+        self.settings.screen_width = self.screen.get_rect().width
+        self.settings.screen_height = self.screen.get_rect().height'''
         self.screen = pygame.display.set_mode(
             (self.settings.screen_width, self.settings.screen_height))
         pygame.display.set_caption("Alien Invasion")
 
         self.ship = Ship(self)
+        self.bullets = pygame.sprite.Group()
 
         #Назначение цвета фона
         self.bg_color = (230, 230, 230)
@@ -26,6 +30,7 @@ class AlienInvasion:
         while True:
             self._check_events()
             self.ship.update()
+            self.bullet.update()
             self._update_screen()
 
 
