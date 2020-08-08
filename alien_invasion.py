9a79ae2843a39a90bc6f29eed5d75fc47e665d29
SStefanovskyi <sstefanovskiy@gmail.com>
2020-08-08 14:53:24 +0300

    add shot processing

--- alien_invasion.py
+++ alien_invasion.py
@@ -4,6 +4,7 @@ import pygame
 
 from settings import Settings
 from ship import Ship
+from bullet import Bullet
 
 class AlienInvasion:
     #Клас для управления ресурсами и поведением игры
@@ -30,7 +31,7 @@ class AlienInvasion:
         while True:
             self._check_events()
             self.ship.update()
-            self.bullet.update()
+            self.bullets.update()
             self._update_screen()
 
 
@@ -52,6 +53,8 @@ class AlienInvasion:
             self.ship.moving_left = True
         elif event.key == pygame.K_q:
             sys.exit()
+        elif event.key == pygame.K_SPACE:
+            self._fire_bullet()
 
     def _check_keyup_events(self, event):
         '''Реагирует на отпускание клавиш'''
@@ -60,10 +63,17 @@ class AlienInvasion:
         elif event.key == pygame.K_LEFT:
             self.ship.moving_left = False
 
+    def _fire_bullet(self):
+        '''Создание нового снаряда и включение его в группу bullets'''
+        new_bullet = Bullet(self)
+        self.bullets.add(new_bullet)
+
     def _update_screen(self):
         # Обновляет изображения на экране и отображает новый экран
         self.screen.fill(self.settings.bg_color)
         self.ship.blitme()
+        for bullet in self.bullets.sprites():
+            bullet.draw_bullet()
 
         pygame.display.flip()
 
