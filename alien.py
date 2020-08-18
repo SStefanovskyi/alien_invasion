4f96f813086415fbaa5aa9460147013b7087f0e4
SStefanovskyi <sstefanovskiy@gmail.com>
2020-08-18 20:33:46 +0300

    add check edges

--- alien.py
+++ alien.py
@@ -21,7 +21,13 @@ class Alien(Sprite):
         #Сохранение точной горизонтальной позиции пришельца
         self.x = float(self.rect.x)
 
+    def check_edges(self):
+        '''Возвращает True, если пришелец находится у края экрана'''
+        screen_rect = self.screen.get_rect()
+        if self.rect.right >=screen_rect.right or self.rect.left <= 0:
+            return True
+
     def update(self):
-        '''Перемещает пришельца вправо'''
-        self.x += self.settings.alien_speed
+        '''Перемещает пришельца влево или вправо'''
+        self.x += (self.settings.alien_speed * self.settings.fleet_direction)
         self.rect.x = self.x
