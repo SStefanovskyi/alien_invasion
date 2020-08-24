9767061783a7e1cc4156f12a2fe88e752423a7c7
SStefanovskyi <sstefanovskiy@gmail.com>
2020-08-24 13:52:31 +0300

    refactor update bullets

--- alien_invasion.py
+++ alien_invasion.py
@@ -131,8 +131,11 @@ class AlienInvasion:
             if bullet.rect.bottom <= 0:
                 self.bullets.remove(bullet)
 
-        #Проверка попаданий в пришельцев
-        #При обнаружении попадания удалить снаряд и пришельца
+        self._check_bullet_alien_collisions()
+
+    def _check_bullet_alien_collisions(self):
+        '''Обработка коллизий снарядов с пришельцами'''
+        #Удаление снарядов и пришельцев, участвующтх в коллизиях
         collisions = pygame.sprite.groupcollide(
             self.bullets, self.aliens, True, True)
 
--- settings.py
+++ settings.py
@@ -12,10 +12,13 @@ class Settings():
         self.ship_speed = 1.5
 
         #Настройки пришельцев
-        self.alien_speed = 1.0
+        self.alien_speed = 1
+        self.fleet_drop_speed = 10
+        #fleet_direction = 1 обозначает движение вправо, а -1 влево
+        self.fleet_direction = 1
 
         #Параметры снаряда
-        self.bullet_speed = 1
+        self.bullet_speed = 1.5
         self.bullet_width = 3
         self.bullet_height = 15
         self.bullet_collor = (60, 60, 60)
