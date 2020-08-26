614afec6f9aece84ee3acd28773618fbdfaf9b3b
SStefanovskyi <sstefanovskiy@gmail.com>
2020-08-26 17:31:36 +0300

    add game statistics file

---
+++ game_stats.py
@@ -0,0 +1,11 @@
+class GameStats():
+    '''Отслеживание статистики для игры Alien Invasion'''
+
+    def __init__(self, ai_game):
+        '''Инициализирует статистику'''
+        self.settings = ai_game.settings
+        self.reset_stats()
+
+    def reset_stats(self):
+        '''Инициализирует статистику, имеющуюся в ходе игры'''
+        self.ships_left = self.settings.ship_limit
