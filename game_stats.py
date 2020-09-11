29cbb8869f421bf46aac6a6a5d6398cb477c57d6
SStefanovskyi <sstefanovskiy@gmail.com>
2020-09-11 19:00:55 +0300

    add score atribute

--- game_stats.py
+++ game_stats.py
@@ -12,3 +12,4 @@ class GameStats():
     def reset_stats(self):
         '''Инициализирует статистику, имеющуюся в ходе игры'''
         self.ships_left = self.settings.ship_limit
+        self.score = 0
