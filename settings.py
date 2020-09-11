076fcca04724325d64129e0d9f2baa0ffae29f63
SStefanovskyi <sstefanovskiy@gmail.com>
2020-09-11 18:58:11 +0300

    add speed settings, initialize_dynamic_settings method

--- settings.py
+++ settings.py
@@ -2,25 +2,31 @@ class Settings():
     # Клас для хранения всех настроек игры Alien Invasion
 
     def __init__(self):
-        # Инициализирует настройки игры
+        # Инициализирует статические настройки игры
         # Параметры экрана
         self.screen_width = 1200
         self.screen_height = 800
         self.bg_color = (230, 230, 230)
 
         #настройки корабля
-        self.ship_speed = 1.5
         self.ship_limit = 3
 
-        #Настройки пришельцев
-        self.alien_speed = 1
-        self.fleet_drop_speed = 10
-        #fleet_direction = 1 обозначает движение вправо, а -1 влево
-        self.fleet_direction = 1
-
         #Параметры снаряда
-        self.bullet_speed = 1.5
         self.bullet_width = 3
         self.bullet_height = 15
         self.bullet_collor = (60, 60, 60)
         self.bullets_allowed = 3
+
+        # Темп ускорения игры
+        self.speedup_scale = 1.1
+
+        self.initialize_dynamic_settings()
+
+    def initialize_dynamic_settings(self):
+        """Инициализирует настройки, изменяющиеся в ходе игры"""
+        self.ship_speed_factor = 1.5
+        self.bullet_speed_factor = 3.0
+        self.alien_speed_factor = 1.0
+
+        # fleet_direction = 1 обозначает движение вправо, а -1 влево
+        self.fleet_direction = 1
