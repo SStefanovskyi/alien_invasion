da74b400e644bb5713994b7f952f0e068db9ffea
SStefanovskyi <sstefanovskiy@gmail.com>
2020-09-14 18:22:24 +0300

    add prep_score method, score get position

--- scoreboard.py
+++ scoreboard.py
@@ -15,3 +15,14 @@ class Scoreboard():
         self.font = pygame.font.SysFont(None, 48)
         #Подготовка исходного изображения
         self.prep_score()
+
+    def prep_score(self):
+        """Преобразует текущий счет в графическое изображение"""
+        score_str = str(self.stats.score)
+        self.score_image = self.font.render(score_str, True, self.text_color,
+                                            self.settings.bg_color)
+
+        # Вывод счета в правой верхней части экрана
+        self.score_rect = self.score_image.get_rect()
+        self.score_rect.right = self.screen_rect.right - 20
+        self.score_rect.top = 20
